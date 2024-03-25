e91ec55163b5227342a72e70ddb6f42784dbcb12
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-03-25 14:44:48 +0800

    update 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,7 +1,18 @@
 const NotFound = () => {
   return (
     <>
-      <h1>404</h1>
+      <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+          <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+            <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+              404
+            </div>
+            <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+              Halaman Tidak Ditemukan
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
